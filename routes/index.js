7600dea2be2975c40399b1e7a9e6fec74322882f
Johinner Madariaga <johimadacor@gmail.com>
2022-12-18 00:26:29 -0500

    modo oscuro

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Johinner-MC.com' });
 });
 
 // router.get('/notas', function(req, res, next) {
